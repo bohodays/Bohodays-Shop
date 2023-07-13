73f3fcbf563ca2523f84df68d456c5a93f352d5f
bohodays <bohodays0316@gmail.com>
2023-07-14 00:04:08 +0900

    design : Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,18 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPencilFill } from "react-icons/bs";
 
 const Navbar = () => {
   return (
-    <header>
-      <Link to={"/"}>
+    <header className="flex justify-between border-b border-gray-300 p-2">
+      <Link to={"/"} className="flex items-center text-4xl text-brand">
         <FiShoppingBag />
         <h1>Bohodays</h1>
       </Link>
-      <nav>
+      <nav className="flex items-center gap-4 font-semibold">
         <Link to={"/products"}>Products</Link>
         <Link to={"/carts"}>Carts</Link>
-        <Link to={"/products/new"}>
+        <Link to={"/products/new"} className="text-2xl">
           <BsFillPencilFill />
         </Link>
         <button>Login</button>
