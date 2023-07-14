5449a39036a758d68a4f0feb26f25978ee75dd9b
bohodays <bohodays0316@gmail.com>
2023-07-15 01:12:12 +0900

    update : firebase key 변수화

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -2,15 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  // authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  // databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
-  // projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  apiKey: "AIzaSyCjnr9U4XYuUgQVWjP02EVvvtK_Nd546Po",
-  authDomain: "bohodays-893be.firebaseapp.com",
-  databaseURL:
-    "https://bohodays-893be-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "bohodays-893be",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
