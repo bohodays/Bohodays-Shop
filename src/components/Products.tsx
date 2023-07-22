5a93715245e008ae7a0f02756a6e41af0f5f1b7d
bohodays <bohodays0316@gmail.com>
2023-07-22 23:28:13 +0900

    design : Products, Banner UI 작업

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -13,7 +13,7 @@ const Products = () => {
     <>
       {isLoading && <p>Loading...</p>}
       {error && <p>error...</p>}
-      <ul>
+      <ul className="grid grid-cols-1 md:grid-cols-3 lg:gird-cols-4 gap-4 p-4">
         {products &&
           products.map((product: any) => (
             <ProductCard key={product.id} product={product}></ProductCard>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
       colors: {
         brand: "#F96162",
       },
+      backgroundImage: {
+        banner: `url('../public/images/banner.jpg')`,
+      },
     },
   },
   plugins: [],
